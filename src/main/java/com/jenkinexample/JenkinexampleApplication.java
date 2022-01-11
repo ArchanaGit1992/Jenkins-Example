866f5960b20e097def993b15ba94321b2486adf0
ArchanaGit1992 <arkshinde@gmail.com>
2022-01-11 20:31:37 +0530

    initiating project

---
+++ src/main/java/com/jenkinexample/JenkinexampleApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinexample/JenkinexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
