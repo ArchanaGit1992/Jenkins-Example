ddcd309498a470909f3a82e6f8de72ed2c5319fc
ArchanaGit1992 <arkshinde@gmail.com>
2022-01-11 20:39:16 +0530

    configure index page

--- src/main/java/com/jenkinexample/JenkinexampleApplication.java
+++ src/main/java/com/jenkinexample/JenkinexampleApplication.java
@@ -8,6 +8,7 @@ public class JenkinexampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinexampleApplication.class, args);
+		System.out.println("heyyyyyyyyyyy");
 	}
 
 }
